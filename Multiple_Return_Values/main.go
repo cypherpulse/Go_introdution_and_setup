5023877b2a3d3aeb04cf00df9fb8c09c5c3fcb26
cypherpulse <johnbradill67@gmail.com>
2026-05-27 06:11:50 +0300

    Refactor main function to improve readability by grouping getInitials calls

--- Multiple_Return_Values/main.go
+++ Multiple_Return_Values/main.go
@@ -24,4 +24,10 @@ func getInitials(n string) (string, string) {
 func main() {
 	fn1, sn1 := getInitials("tifa lockhart")
 	fmt.Printf("First Name Initial: %s, Second Name Initial: %s\n", fn1, sn1)
+
+	fn2, sn2 := getInitials("cloud strife")
+	fmt.Printf("First Name Initial: %s, Second Name Initial: %s\n", fn2, sn2)
+
+	fn3, sn3 := getInitials("Aerith")
+	fmt.Printf("First Name Initial: %s, Second Name Initial: %s\n", fn3, sn3)
 }
